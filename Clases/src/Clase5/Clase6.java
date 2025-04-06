3aa1c6a5cdfd61e871288dd1538a535ea35a67b6
carlospomadev_ <c.poma.ramirez@gmail.com>
2025-04-06 22:46:22 +0200

    actualización

---
+++ Clases/src/Clase5/Clase6.java
@@ -0,0 +1,37 @@
+package Clase6;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Clase6 {
+    public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    try{
+//        System.out.println("Ingresame un número: ");
+//        int numero = scanner.nextInt();
+//        System.out.println("El número ingresado es: " + numero);
+//    } catch (Exception e) {
+//        System.out.println("Número invalido");
+//    } finally {
+//        scanner.close();
+//        System.out.println("Porgrama finalizado");
+//    }
+//    }
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+            System.out.println("Ingresame un número: ");
+            int numero = scanner.nextInt();
+            if (numero > 5) {
+                System.out.println("El número es mayor que 5, número ingresado: " + numero);
+            } else {
+                System.out.println("Número invalido, número ingresado: " + numero);
+            }
+        } catch (Exception e) {
+            System.out.println("Dato ingresado no válido");
+        } finally {
+            scanner.close();
+            System.out.println("Porgrama finalizado");
+        }
+    }
+}
