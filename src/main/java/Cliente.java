024f03ed27fc133e5836c9e0117c93c575832d18
carlospomadev_ <c.poma.ramirez@gmail.com>
2025-08-04 01:03:27 +0200

    AVANCE 1

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+    private String nombre;
+    private String dni;
+    private  String telefono;
+    private String email;
+}
+public Cliente(String nombre, String dni, String telefono, String email){
+    this.nombre = nombre;
+    this.dni = dni;
+    this.telefono = telefono;
+    this.email = email;
+}
+public String getNombre(){
+    return nombre;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
